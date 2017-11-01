1ca96650be8ebcfb06caa5325c976d8a1bed855b
Eric Liu <liueric101@gmail.com>
2017-11-01 00:47:59 -0700

    Added arduino files

---
+++ neopixel/neopixel.ino
@@ -0,0 +1,29 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 8
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // Some example procedures showing how to display to the pixels:
+  Serial.print(strip.numPixels());
+  colorWipe(strip.Color(255, 0, 0), 500); // Red
+  colorWipe(strip.Color(0, 255, 0), 500); // Green
+  colorWipe(strip.Color(0, 0, 255), 500); // Blue
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  strip.setPixelColor(0, c);
+  strip.show();
+  delay(wait);
+}
+
+
